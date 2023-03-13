0532badf2e7366032127cc1758408743e915c806
edwin0003 <edwincrespomarin@gmail.com>
2023-03-13 17:15:08 -0400

    add counter pokemon

--- myApp/src/pokeApp/components/PokeApp.jsx
+++ myApp/src/pokeApp/components/PokeApp.jsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "../pokeApp.css";
 import { apiUrl } from "../../constants";
-import PokeCard from "./PokeCard";
 import PokeSearch from "./PokeSearch";
 import PokeList from "./PokeList";
 function PokeApp() {
   const [pokemonList, setPokemonList] = useState([]);
   const [searchPokemon, setSearchPokemon] = useState("");
+  const [pokecount, setPokecount] = useState([]);
 
   const onLoadpokemon = async () => {
     const response = await fetch(apiUrl);
     const data = await response.json();
     setPokemonList(data.results);
+    setPokecount(data.results.length);
+
   };
   useEffect(() => {
     onLoadpokemon();
@@ -26,7 +28,18 @@ function PokeApp() {
 
   const filteredPokemons = pokemonList.filter((pokemon) =>
     pokemon.name.includes(searchPokemon.toLowerCase())
+    
   );
+ 
+  const countResult =() =>{
+    if (filteredPokemons) {
+      return filteredPokemons.length
+    }else{
+      const result = pokecount
+    return result;
+    }
+    
+  }
 
   return (
     <div style={{ display: "grid", gap: "20px" }}>
@@ -43,17 +56,9 @@ function PokeApp() {
           </div>
         </div>
       </div>
-      {/* <div className="responsive-card">
-        {(searchPokemon === "" ? pokemonList : filteredPokemons).map(
-          (pokemon) => (
-            <PokeCard
-              key={pokemon.name}
-              pokemon={pokemon}
-              getPokemonNumber={() => getPokemonNumber(pokemon.url)}
-            />
-          )
-        )}
-      </div> */}
+      <div>
+        <h2>Result: {countResult()}</h2>
+      </div>
       <PokeList filteredPokemons={filteredPokemons} pokemonList={pokemonList} searchPokemon={searchPokemon}/>
     </div>
   );
